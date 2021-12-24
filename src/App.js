4f104b8a94460f0838cf8b8484457154f2ff2beb
vicky9462 <vgguptaalwar10@gmail.com>
2021-12-24 18:02:45 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Login from './Login';
+
+function App() {
+  return (
+    
+    <div className="g-signin">
+      <Login />
+    </div>
+  )
+}
+
+export default App;
